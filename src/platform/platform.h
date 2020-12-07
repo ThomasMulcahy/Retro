7c8ddc2a99ccc0e10b2428b200255fea07abc1e5
ThomasMulcahy <tommulcahy98@gmail.com>
2020-12-07 20:29:57 +0000

    Update: Remove Metal and Font - Start work on Cocoa rendering system

--- src/platform/platform.h
+++ src/platform/platform.h
@@ -1,8 +1,6 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
-#include "../ui.h"
-#include "../font.h"
 
 typedef struct _WindowOpt {
     char *title;
@@ -10,6 +8,6 @@ typedef struct _WindowOpt {
     int height;
 } WindowOpt;
 
-int platformRun(WindowOpt *winOptions, UIElement *headElement);
+int platformRun(WindowOpt *winOptions);
 
 #endif
